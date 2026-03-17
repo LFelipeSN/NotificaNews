ffdda510c6effc1fa4c71af9ad5f45799e905b7a
Luiz Felipe n <88946180+LFelipeSN@users.noreply.github.com>
2026-03-17 20:36:52 -0300

    Update Slack URL retrieval method in config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ with open("config.json", "r", encoding="utf-8") as f:
     configuracoes = json.load(f)
 
 urlNewsletter = configuracoes["urls"]["newsletter"]
-urlSlack = configuracoes["urls"]["slack"]
+urlSlack = os.getenv("URL_SLACK") or configuracoes["urls"]["slack"]
 parametrosBusca = configuracoes["parametros"]
 emojis = configuracoes["emojis"]
